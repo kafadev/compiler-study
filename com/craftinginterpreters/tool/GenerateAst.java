8f3237a2bff4321ee6c9934e99be2e8791c74d35
kafadev <89330010+kafadev@users.noreply.github.com>
2025-07-25 07:21:05 -0500

    finished chapter 8 [except for challenges]

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -28,14 +28,18 @@ public class GenerateAst {
 
     // description of the classs we're creating 
     defineAst(outputDir, "Expr", Arrays.asList(
+      "Assign   : Token name, Expr value",
       "Binary   : Expr left, Token operator, Expr right",
       "Grouping : Expr expression",
       "Literal  : Object value",
-      "Unary    : Token operator, Expr right"
+      "Unary    : Token operator, Expr right",
+      "Variable : Token name"
     ));
     defineAst(outputDir, "Stmt", Arrays.asList(
+      "Block      : List<Stmt> statements",
       "Expression : Expr expression",
-      "Print      : Expr expression"
+      "Print      : Expr expression",
+      "Var        : Token name, Expr initializer"
     ));
   }
 
